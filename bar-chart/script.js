33212890782a88ae1bf6a13a0c65c9ccad62afc6
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-05 23:16:44 +0100

    Rewite and pass user story 1 in bar chart

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -1,8 +1,21 @@
+const width = 800;
+const height = 500;
+const padding = 20;
+
+let dataset;
+
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
 fetch(url)
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+  .then((res) => {
+    dataset = res.data;
+
+    drawCanvas();
   });
+
+const drawCanvas = () => {
+  const svg = d3.select("svg");
+  svg.attr("width", width).attr("height", height);
+};
