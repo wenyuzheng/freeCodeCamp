a0cfbb9f6c478e7a56a6caa4241b322df8b17161
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-05 14:17:37 +0100

    Link styles.css and fetch data

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -1,1 +1,8 @@
-console.log(d3);
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
