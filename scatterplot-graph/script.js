8bbc663e600f440373ce0c3e6cf3a42fa10035d2
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-06 15:53:53 +0100

    Scatterplot user story 2-3

--- scatterplot-graph/script.js
+++ scatterplot-graph/script.js
@@ -5,7 +5,7 @@ const url =
 
 const w = 800;
 const h = 500;
-const padding = 40;
+const padding = 50;
 
 const svg = d3.select("svg");
 
@@ -15,7 +15,10 @@ fetch(url)
   .then((response) => response.json())
   .then((res) => {
     dataset = res;
+    console.log(dataset);
+
     drawCanvas();
+    drawAxes();
   });
 
 const drawCanvas = () => {
@@ -30,4 +33,38 @@ const addTitle = () => {
     .attr("id", "title")
     .attr("x", w / 2)
     .attr("y", padding);
+};
+
+const drawAxes = () => {
+  const { xScale, yScale } = generateScales();
+
+  const xAxis = d3.axisBottom(xScale);
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", `translate(0, ${h - padding})`);
+
+  const yAxis = d3.axisLeft(yScale);
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", `translate(${padding}, 0)`);
+};
+
+const generateScales = () => {
+  const years = dataset.map((d) => d["Year"]);
+  const xScale = d3
+    .scaleLinear()
+    .domain([d3.min(years), d3.max(years)])
+    .range([padding, w - padding]);
+
+  const seconds = dataset.map((d) => d["Seconds"]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([d3.min(seconds), d3.max(seconds)])
+    .range([h - padding, padding]);
+
+  return { xScale, yScale };
 };
