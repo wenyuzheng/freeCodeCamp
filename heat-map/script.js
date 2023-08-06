75c426ff145b8d87c58db7d8f371314aa63e9edc
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-06 20:49:13 +0100

    Heat map user story 2

--- heat-map/script.js
+++ heat-map/script.js
@@ -10,4 +10,14 @@ fetch(url)
   .then((res) => {
     dataset = res;
     console.log(dataset);
+    addDescription();
   });
+
+const addDescription = () => {
+  const years = dataset.monthlyVariance.map((e) => e.year);
+  document.getElementById(
+    "description"
+  ).textContent = `Base temperature in ${d3.min(years)} - ${d3.max(years)}: ${
+    dataset.baseTemperature
+  } ℃`;
+};
