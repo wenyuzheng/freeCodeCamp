674d1d1a8f466ba13b3137c20a05362521c66d06
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-10 17:27:51 +0100

    user story 3

--- choropleth-map/script.js
+++ choropleth-map/script.js
@@ -1,18 +1,32 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const countiesUrl =
+const countyURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
-const educationUrl =
+const educationURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 
-let countiesData, edcuationData;
+const svg = d3.select("svg");
 
-d3.json(countiesUrl).then((data) => {
-  countiesData = data;
+let countyData, educationData;
 
-  d3.json(educationUrl).then((data) => {
-    edcuationData = data;
+d3.json(countyURL).then((data) => {
+  countyData = topojson.feature(data, data.objects.counties).features;
 
-    console.log(countiesData, edcuationData);
+  d3.json(educationURL).then((data) => {
+    educationData = data;
+
+    console.log(countyData, educationData);
+
+    drawMap();
   });
 });
+
+const drawMap = () => {
+  svg
+    .selectAll("path")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county");
+};
