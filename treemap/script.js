87bb10868d7912226458e09e6f4d16e70415e5ab
Wenyu Zheng <zhengwy229@gmail.com>
2023-08-12 00:22:18 +0100

    fetch data

--- treemap/script.js
+++ treemap/script.js
@@ -1,3 +1,12 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-console.log(d3);
+const url =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+let dataset;
+
+d3.json(url).then((data) => {
+  dataset = data;
+
+  console.log(dataset);
+});
